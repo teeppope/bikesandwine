098be13df97838d621bf10a745f2ddac46d59e8b
teeppope <terraepop@gmail.com>
2016-10-21 14:58:54 -0700

    get smooth scroll to work

--- jquery.js
+++ jquery.js
@@ -7,4 +7,23 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 
 $(document).ready(function(){
 	console.log('hello');
+
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top - 95  
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
+		if( target === '#about'){
+			$('.about').css("background-color","gold");
+		} else{
+			$('.about').css("background-color","rgb(225,225,225)").delay(1000);
+		}
+	});
+
 });
